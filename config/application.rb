6df89f9d45aa91bd94a913c803f9ab7429c9343d
Rick Martinez <rickmzp@gmail.com>
2013-03-17 17:09:07 -0400

    mongoid > activerecord

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,15 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "rails"
+
+%w(
+  action_controller
+  action_mailer
+  rails/test_unit
+  sprockets
+).each do |framework|
+  require "#{framework}/railtie"
+end
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -51,7 +60,6 @@ module BlueNavy
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
