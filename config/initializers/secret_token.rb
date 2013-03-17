5ab7b6e75e660183fea34cacb39817d72032e1f0
Rick Martinez <rickmzp@gmail.com>
2013-03-17 17:04:29 -0400

    rails gen

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlueNavy::Application.config.secret_token = '54b76d717d35c91aadc5e99981d8eecd1b84956fea4a8e7d145e3edfd8ddb29fbb1909fdf3734e7ec9570e9bcf34f0657a9887178a7653ada42be75f4b167e22'
